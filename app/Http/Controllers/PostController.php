7a399a9464029aee72e1d0f9f312fe9d46b73d7b
LideOP <95317741+LideOP@users.noreply.github.com>
2023-08-16 18:59:10 -0400

    Agregando mi primer commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,40 @@ class PostController extends Controller
      *
      * @return response()
      */
-    public function index(Request $request)
+    public function buscarComentario(Request $request)
+    {
+        $comments = Post::find(1)->comments;
+  
+        dd($comments);
+    }
+    public function buscarPosteo(Request $request)
+    {
+        $post = Comment::find(1)->post;
+  
+        dd($post);
+    }
+    public function agregarComentario(Request $request)
+    {
+        $post = Post::find(1);
+   
+        $comment = new Comment;
+        $comment->comment = "Hi ItSolutionStuff.com";
+           
+        $post = $post->comments()->save($comment);
+    }
+    public function AgregarNuevosComentario(Request $request)
+    {
+        $post = Post::find(1);
+   
+        $comment1 = new Comment;
+        $comment1->comment = "Hi ItSolutionStuff.com Comment 1";
+           
+        $comment2 = new Comment;
+        $comment2->comment = "Hi ItSolutionStuff.com Comment 2";
+           
+        $post = $post->comments()->saveMany([$comment1, $comment2]);
+    }
+    public function postearComentario(Request $request)
     {
         $comment = Comment::find(1);
         $post = Post::find(2);
